15f15d4c650d49fb525a1a1471bf7cb8b92f9365
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-04-15 10:26:41 +0200

    Create alfa 2.0 version

--- app/home/home.js
+++ app/home/home.js
@@ -1,5 +1,5 @@
 const CRAWL_LIMIT = 20; // Number of shows to crawl
-const CRAWL_TV_SHOWS_LIMIT = 2; // Number of TVshows to crawl
+const CRAWL_TV_SHOWS_LIMIT = 20; // Number of TVshows to crawl
 const CRAWL_TV_SHOWS_FAVORITES_LIMIT = 4; // Number of links with the latest episodes for each favorite show
 
 //
