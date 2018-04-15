c851b4d1fd983a0c2f71746053678040b075cfd0
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-04-15 18:21:51 +0200

    New database repository dir: UserData

--- app/home/favorites.js
+++ app/home/favorites.js
@@ -3,12 +3,21 @@
 //
 var ShowCollection = require('vws-js-lib/lib/showCollection');
 var crawler = require('vws-js-lib/lib/crawler');
-var FavoriteRepository = require('favoriteRepository');
+var FavoriteRepository = require('vws-js-lib/lib/favoriteRepository');
+const path = require('path');
+const electron = require('electron');
+
+//
+// Constants
+//
+// Renderer process has to get `app` module via `remote`, whereas the main process can get it directly
+// app.getPath('userData') will return a string of the user's app data directory path.
+const userDataPath = (electron.app || electron.remote.app).getPath('userData');
 
 //
 // Global var
 //
-var favoriteRepository = new FavoriteRepository('vws-db');
+var favoriteRepository = new FavoriteRepository(path.join(userDataPath, 'vws-db'));
 
 //
 // Functions
