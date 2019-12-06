1081b717d70b90c93dada52fa6fd1147fcd9b6cd
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-12-06 15:45:58 +0100

    dontorrent data on show-box component

--- app/main.js
+++ app/main.js
@@ -15,7 +15,8 @@ function createWindow() {
         icon: path.join(__dirname, '../build/icons/64x64.png'),
         webPreferences: {
             nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            webSecurity: false // TODO: Esto lo hago para poder cargar las imagenes de dontorrent.org. Buscare otra alternativa en algun momento 
         }
     })
 
