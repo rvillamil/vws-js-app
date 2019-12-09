2c7093e4101ca5f900dfe703832ff4f0d090ab85
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-12-09 18:36:41 +0100

    Beta version from searcher

--- app/home/constants.js
+++ app/home/constants.js
@@ -2,3 +2,4 @@
 const CRAWL_LIMIT = 40 // Number of shows to crawl
 const CRAWL_TV_SHOWS_LIMIT = 30 // Number of TVshows to crawl
 const CRAWL_TV_SHOWS_FAVORITES_LIMIT = 6 // Number of links with the latest episodes for each favorite show
+const SEARCHER_MAX_RESULTS = 3 // Max results on searching shows
--- app/home/searcher.js
+++ app/home/searcher.js
@@ -1,3 +1,55 @@
+/* eslint-disable no-console */
+//
+// npm modules required
+//
+var searcher = require('vws-js-lib/lib/searcher')
+
 function loadAndRenderShowSearch(htmlElementID) {
-    document.getElementById(htmlElementID).innerHTML = 'BUSCADOR'
+
+    //document.getElementById("search-box").value = ''
+    //document.getElementById("show-search-results").innerHTML = ''
+    //
+    // VER: https://developer.mozilla.org/es/docs/Web/HTML/Elemento/input/search
+    //
+    document.getElementById(htmlElementID).innerHTML =
+        `<div class="show-searcher">
+            <input type="search" id="search-box" name="q" placeholder="Buscar películas o series ..." />
+            <button id='search-button' onclick="searchShows()">Buscar</button>
+        </div>
+        <div id="show-search-results">
+        </div>
+        `
+}
+
+function searchShows() {
+    const textSearch = document.getElementById("search-box").value
+
+    if (textSearch) {
+        //document.getElementById("show-search-results").innerHTML = ''
+
+        var modalWindow = showModalWindow(
+            'Espere por favor..',
+            'Buscando ...',
+            ''
+        )
+
+        return searcher
+            .searchShows(textSearch, SEARCHER_MAX_RESULTS, searchResult => {
+                console.log(`onSearchResult !!  --> ${JSON.stringify(searchResult)}\n\n`)
+                if (searchResult.type == 'film') {
+                    document.getElementById("show-search-results").innerHTML += renderFilm(searchResult.show)
+                } else if (searchResult.type == 'tvshowcollection') {
+                    document.getElementById("show-search-results").innerHTML += renderShowCollectionBox(searchResult.show, 1)
+                }
+            })
+            .then(searchShows => {
+                console.log(
+                    `searcher.js - searchShows - results found: ${searchShows.length}`
+                )
+                closeModalWindow(modalWindow)
+            })
+            .catch(err => {
+                onLoadAndRenderShowsError(htmlElementID, modalWindow, err)
+            })
+    }
 }
